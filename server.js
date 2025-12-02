cce2619d9212c44e431393561408bd9e19ddbd9f
Martina Papp <martinapapp0@gmail.com>
2025-12-02 12:44:02 +0100

    add more param route, refactor code, add utils directory

--- server.js
+++ server.js
@@ -1,38 +1,48 @@
 import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { sendJSONResponse } from './utils/jsonResponse.js'
+import { getDataByPathParams } from './utils/params.js'
 
 const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB()
 
+/*
+Challenge:
+  1. Create a util function to filter data.
+  2. Wire it up and delete unneeded code.
+*/
+
+
+
   if (req.url === '/api' && req.method === 'GET') {
 
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(destinations))
+    sendJSONResponse(res, 200, destinations)
 
   } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
 
     const continent = req.url.split('/').pop()
-    const filteredData = destinations.filter((destination) => {
-      return destination.continent.toLowerCase() === continent.toLowerCase()
-    })
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(filteredData))
+    const filteredData = getDataByPathParams(destinations, 'continent', continent)
+    sendJSONResponse(res, 200, filteredData)
 
-  } else {
+  } else if (req.url.startsWith('/api/country') && req.method === 'GET') {
+
+    const country = req.url.split('/').pop()
+    const filteredData = getDataByPathParams(destinations, 'country', country)
+    sendJSONResponse(res, 200, filteredData)
+
+  } 
+  
+  else {
 
     res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.end(JSON.stringify({
+    sendJSONResponse(res, 404, ({
       error: "not found",
       message: "The requested route does not exist"
-    })
-    )
+    }))   
+
   }
-  
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
